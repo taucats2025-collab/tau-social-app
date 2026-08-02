6999a73cff638817237d14c4ae15a0279404e4d4
taucats2025-collab <tau.cats.2025@gmail.com>
2026-08-02 15:44:48 +0300

    Update tau-social-app.py

--- tau-social-app.py
+++ tau-social-app.py
@@ -32,7 +32,7 @@ if st.button("צור פוסטים! ✨"):
 {user_text}
 """
                 response = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model='gemini-1.5-flash',
                     contents=prompt,
                 )
 
